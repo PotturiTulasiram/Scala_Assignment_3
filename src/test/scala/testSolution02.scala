475d5c9ad88cb9c1d9bb9c9bb42cb96ea0badd49
PotturiTulasiram <75593659+PotturiTulasiram@users.noreply.github.com>
2021-12-10 09:54:10 +0530

    Delete src/test/scala directory

--- src/test/scala/testSolution02.scala
+++
@@ -1,8 +0,0 @@
-import org.scalatest.funsuite.AnyFunSuite
-
-class testSolution02 extends AnyFunSuite {
-  test("Solution02"){
-    assert(Solution02.Oddpos(List(1,2,3,4,5,6)) === List(2,4,6))
-    assert(Solution02.Oddpos(List(3,525,231,62,54,433)) === List(525,62,433))
-  }
-}
